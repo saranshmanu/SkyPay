40e22bcee5cc27d27735f9002c961a5b44371d09
Saransh Mittal <saranshmanu@yahoo.co.in>
2017-05-10 20:19:45 +0530

    Initial Commit

---
+++ SkyPay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SkyPay
+//
+//  Created by Saransh Mittal on 10/05/17.
+//  Copyright © 2017 Saransh Mittal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
